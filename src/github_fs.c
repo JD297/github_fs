7f6d7f7dd60e6202cdde6b27ab33ae4a66694f0f
JD297 <jan.dommasch297@gmail.com>
2025-07-23 21:01:18 +0200

    Added dynamic repository listing

--- src/github_fs.c
+++ src/github_fs.c
@@ -7,73 +7,136 @@
 #include <curl/curl.h>
 #include <json-c/json.h>
 
-struct json_object* read_json_from_file(FILE* file)
-{
-	char* json_string = NULL;
+typedef struct {
+	const char *value;
+	size_t len;
+} sv_t;
 
+typedef struct {
+	sv_t user;
+	sv_t repo;
+	sv_t branch;
+	sv_t path;
+} fs_github_path;
+
+int fs_github_path_parse_from_path(fs_github_path *fgp, const char *path)
+{
+	fgp->user.value = NULL;
+	fgp->repo.value = NULL;
+	fgp->branch.value = NULL;
+	fgp->path.value = NULL;
+
+	const char *current = path + 1;
+	const char *next;
+	size_t index = 0;
+
+	while (*current != '\0') {
+		next = current;
+		while (*next != '\0' && *next != '/') {
+			++next;
+		}
+
+		switch (index) {
+			case 0: {
+				fgp->user.value = current;
+				fgp->user.len = next - current;
+			} break;
+			case 1: {
+				fgp->repo.value = current;
+				fgp->repo.len = next - current;
+			} break;
+			case 2: {
+				fgp->branch.value = current;
+				fgp->branch.len = next - current;
+			} break;
+			case 3: {
+				fgp->user.value = current;
+				fgp->user.len = strlen(current);
+
+				return 0;
+			} break;
+		}
+
+		index++;
+
+		current = (*next == '/') ? next + 1 : next;
+	}
+
+	return 0;
+}
+
+json_object *github_api_users_repos(sv_t *user)
+{
+	FILE *file = tmpfile();
+
+	CURL *curl = curl_easy_init();
+
+	const char *fs_github_users_repos_url_p1 = "https://api.github.com/users/";
+	const char *fs_github_users_repos_url_p2 = "/repos";
+
+	// TODO check for errors
+	char *fs_github_users_repos_url = calloc(user->len + strlen(fs_github_users_repos_url_p1) + strlen(fs_github_users_repos_url_p2) + 1, sizeof(char));
+
+	strcpy(fs_github_users_repos_url, fs_github_users_repos_url_p1);
+	strncat(fs_github_users_repos_url, user->value, user->len);
+	strcat(fs_github_users_repos_url, fs_github_users_repos_url_p2);
+
+	curl_easy_setopt(curl, CURLOPT_URL, fs_github_users_repos_url);
+
+	curl_easy_setopt(curl, CURLOPT_USERAGENT, "github_fs");
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+
+	curl_easy_perform(curl);
+
+	rewind(file);
+
+	char *json_str = NULL;
 	const size_t block = 4096;
 	size_t nread_total = 0;
 
 	do {
-		json_string = (char *)realloc(json_string, nread_total + block);
+		json_str = (char *)realloc(json_str, nread_total + block);
 
-		nread_total += fread(json_string + nread_total, sizeof(char), block, file);
-
-		//printf(">>>%s<<<\n", json_string);
+		nread_total += fread(json_str + nread_total, sizeof(char), block, file);
 	} while (feof(file) == 0);
 
-	struct json_object* json_obj = json_tokener_parse(json_string);
+	json_object *root = json_tokener_parse(json_str);
 
-	//printf(">>\n%s\n<<\n", json_string);
+	free(json_str);
 
-	//free(json_string);
-
-	return json_obj;
+	return root;
 }
-
-/*
-size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
-{
-
-}
-*/
 
 static int fs_readdir(const char *path, void *data, fuse_fill_dir_t filler, off_t off, struct fuse_file_info *ffi)
 {
 	(void)off;
 	(void)ffi;
 
-	printf("FILE: %s\n", path);
+	fs_github_path fgp;
+	int fgp_parse_result;
 
-	if (strcmp(path, "/jd297") == 0) {
+	if ((fgp_parse_result = fs_github_path_parse_from_path(&fgp, path)) != 0) {
+		return fgp_parse_result;
+	}
+
+	/* USERS */
+	if (fgp.user.value == NULL) {
+		// curl https://api.github.com/search/users?q=jd297
+
+		filler(data, ".", NULL, 0);
+		filler(data, "..", NULL, 0);
+		filler(data, "jd297", NULL, 0); // TODO HARD
+
+		return 0;
+	}
+
+	/* REPOSITORIES */
+	if (fgp.user.value != NULL && fgp.repo.value == NULL) {
 		if (off != 0) { // TODO: research this! but the hack for now works! :)
 			return 0;
 		}
 
-		FILE *file = tmpfile();
-
-		CURL *curl = curl_easy_init();
-		curl_easy_setopt(curl, CURLOPT_URL, "https://api.github.com/users/JD297/repos");
-		curl_easy_setopt(curl, CURLOPT_USERAGENT, "github_fs");
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
-
-		curl_easy_perform(curl);
-
-		rewind(file);
-
-		char *json_str = NULL;
-		const size_t block = 4096;
-		size_t nread_total = 0;
-
-		do {
-			json_str = (char *)realloc(json_str, nread_total + block);
-
-			nread_total += fread(json_str + nread_total, sizeof(char), block, file);
-		} while (feof(file) == 0);
-
-		json_object *root = json_tokener_parse(json_str);
-
-		free(json_str);
+		json_object *root = github_api_users_repos(&fgp.user);
 
 		size_t array_length = json_object_array_length(root);
 
@@ -83,7 +146,6 @@ static int fs_readdir(const char *path, void *data, fuse_fill_dir_t filler, off_
 			struct json_object* name_obj;
 
 			if (json_object_object_get_ex(json_obj, "name", &name_obj)) {
-				// printf("NAME: %s\n", json_object_get_string(name_obj));
 				filler(data, json_object_get_string(name_obj), NULL, 0);
 			}
 		}
@@ -93,82 +155,51 @@ static int fs_readdir(const char *path, void *data, fuse_fill_dir_t filler, off_
 		return 0;
 	}
 
-	if (strcmp(path, "/") != 0) {
-		return ENOENT;
-	}
-
-	// curl https://api.github.com/search/users?q=jd297
-
-	filler(data, ".", NULL, 0);
-	filler(data, "..", NULL, 0);
-	filler(data, "jd297", NULL, 0);
-
-
-	return 0;
+	return ENOENT;
 }
-
-/*
-static int fs_read(const char *path, char *buf, size_t size, off_t off, struct fuse_file_info *ffi)
-{
-	(void)path;
-	(void)ffi;
-
-	size_t len;
-	const char *file_contents = "fuse filesystem example\n";
-
-	len = strlen(file_contents);
-
-	if (off < len) {
-		if (off + size > len) {
-			size = len - off;
-		}
-
-		memcpy(buf, file_contents + off, size);
-	} else {
-		size = 0;
-	}
-
-	return size;
-}
-
-static int fs_open(const char *path, struct fuse_file_info *ffi)
-{
-	return 0;
-
-	if (strncmp(path, "/file", 10) != 0) {
-		return -ENOENT;
-	}
-
-	if ((ffi->flags & 3) != O_RDONLY) {
-		return -EACCES;
-	}
-
-	return 0;
-}
-*/
 
 static int fs_getattr(const char *path, struct stat *st)
 {
-	if (strcmp(path, "/") == 0) {
+	// printf("  DEBUG: fs_getattr: %s", path);
+
+	fs_github_path fgp;
+	int fgp_parse_result;
+
+	if ((fgp_parse_result = fs_github_path_parse_from_path(&fgp, path)) != 0) {
+		return fgp_parse_result;
+	}
+
+	printf("\nUSER:>>%s<<(%zu)\n", fgp.user.value, fgp.user.len);
+	printf("\nREPO:>>%s<<(%zu)\n", fgp.repo.value, fgp.repo.len);
+
+	// if (strcmp(path, "/") == 0) {
+	if (fgp.user.value == NULL) {
 		st->st_blksize = 512;
 		st->st_mode = S_IFDIR | 0555;
 		st->st_nlink = 3;
+
+		return 0;
 	}
-	else if (strcmp(path, "/jd297") == 0) {
+
+	//if (strcmp(path, "/jd297") == 0) {
+	if (fgp.user.value != NULL && fgp.repo.value == NULL) {
+		// TODO check if user exists
+		// 	check if cache hits
+		//  else lookup from api
+		// if not exists then -ENOENT
+	
 		st->st_mode = S_IFDIR | 0555;
 		st->st_blksize = 512;
 		st->st_nlink = 2;
-	} else {
-		return -ENOENT;
+
+		return 0;
 	}
 
-	return 0;
+	return -ENOENT;
 }
 
 struct fuse_operations fsops = {
 	.readdir = fs_readdir,
-	//.read = fs_read,
-	//.open = fs_open,
 	.getattr = fs_getattr,
 };
 
